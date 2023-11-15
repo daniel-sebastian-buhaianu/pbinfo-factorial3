419d6d71cef10f51e05e7949e7feb65ce4659197
Daniel-Sebastian Buhaianu <128343993+daniel-sebastian-buhaianu@users.noreply.github.com>
2023-11-15 05:22:38 +0200

    Create factorial3.cpp

---
+++ factorial3.cpp
@@ -0,0 +1,31 @@
+#include <fstream>
+#define MAX_N 100001
+using namespace std;
+int exponent[MAX_N];
+void descompuneInFactoriPrimi(int);
+int main()
+{
+	int n, i, suma;
+	ifstream fin("factorial3.in");
+	fin >> n;
+	fin.close();
+	for (i = 2; i <= n; i++)
+		descompuneInFactoriPrimi(i);
+	for (suma = 0, i = 2; i <= n; i++)
+		suma += exponent[i];
+	ofstream fout("factorial3.out");
+	fout << suma;
+	fout.close();
+	return 0;
+}
+void descompuneInFactoriPrimi(int nr)
+{
+	for (int d = 2; d*d <= nr; d++)
+	{
+		int p;
+		for (p = 0; nr % d == 0; nr /= d, p++);
+		exponent[d] += p;
+	}
+	if (nr > 1)
+		exponent[nr] += 1;
+}
